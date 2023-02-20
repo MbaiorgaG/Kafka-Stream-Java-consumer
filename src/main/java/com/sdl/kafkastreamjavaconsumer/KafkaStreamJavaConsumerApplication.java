46349fefc189e01064ed4651d91c694b1412542c
Gabriel Simon Mbaiorga <msgtoolz@gmail.com>
2023-02-20 16:25:41 +0100

    create kafka consumer project

---
+++ src/main/java/com/sdl/kafkastreamjavaconsumer/KafkaStreamJavaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sdl.kafkastreamjavaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamJavaConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaStreamJavaConsumerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sdl/kafkastreamjavaconsumer/KafkaStreamJavaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdl.kafkastreamjavaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamJavaConsumerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
